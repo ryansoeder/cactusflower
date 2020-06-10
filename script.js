fad51855b348d24d5d4b2f1dcbd2379e0c8400ea
Ryan Soeder <mr.soeder@gmail.com>
2020-06-10 16:16:49 -0400

    added comments to lightbox js for learning purposes

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
+// Lightbox for Pawpaw Corner gallery. Notes for learning purposes.
 
-const images = document.querySelectorAll('img')
-images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightbox.classList.add('active');
-        const img = document.createElement('img');
-        img.src = image.src;
-        while(lightbox.firstChild){
-            lightbox.removeChild(lightbox.firstChild);
+const lightbox = document.createElement('div'); // creates a div element and stores it in lightbox variable
+lightbox.id = 'lightbox'; // gives that div an id of '#lightbox'
+document.body.appendChild(lightbox); // attaches that div to the body tag in the html
+
+const images = document.querySelectorAll('img') // stores all images on the page in 'images' variable
+images.forEach(image => { // for every image on the page...
+    image.addEventListener('click', e => { // listen for it to be clicked on and...
+        lightbox.classList.add('active'); // change the class to 'actice' (see CSS)
+        const img = document.createElement('img'); // create in img element and store it in 'img' variable
+        img.src = image.src; // make the new image the same as the one on the page
+        while(lightbox.firstChild){ // if #lightbox has anything in it (children)...
+            lightbox.removeChild(lightbox.firstChild); // remove it before placing anything else in #lightbox
         }
-        lightbox.appendChild(img);
+        lightbox.appendChild(img); // put the new image (same origin as original) into #lightbox
     })
 });
 
-lightbox.addEventListener('click', e => {
-    if (e.target !== e.currentTarget) return;
-    lightbox.classList.remove('active');
+lightbox.addEventListener('click', e => { // when you click on #lightbox when is active on the web page...
+    if (e.target !== e.currentTarget) return; // and you're not clicking on the picture you clicked to make #lightbox active in the first place...
+    lightbox.classList.remove('active'); // turn off .active state [(close #lightbox) see CSS]
 });
